515a372acdb0ea97e21f145178109c3442e5a99b
Luka_Radulovic <luka.radulovic.011@gmail.com>
2022-06-18 19:36:26 +0200

    Added proper dependancies

--- XamarinTestApp/App.xaml.cs
+++ XamarinTestApp/App.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 namespace XamarinTestApp
 {
@@ -15,6 +18,11 @@ namespace XamarinTestApp
 
         protected override void OnStart()
         {
+            AppCenter.Start("android={Your app secret here};" +
+                  "uwp={Your UWP App secret here};" +
+                  "ios={Your iOS App secret here};" +
+                  "macos={Your macOS App secret here};",
+                  typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep()
