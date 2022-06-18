a9e3d9cd315ae1e712df7aa573d3cbc9a3b7fb41
Luka_Radulovic <luka.radulovic.011@gmail.com>
2022-06-18 18:55:02 +0200

    initial commit

---
+++ XamarinTestApp/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinTestApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        int count = 0;
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            count++;
+            ((Button)sender).Text = $"You clicked {count} times.";
+            if (count == 10)
+            {
+                ((Button)sender).Text = $"Antistress process finished!";
+                System.Diagnostics.Process.GetCurrentProcess().CloseMainWindow();
+            }
+        }
+
+    }
+}
